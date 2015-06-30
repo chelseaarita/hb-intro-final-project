c0f8e63802f6f24b2a3d14181d12f3c85fdea12b
Chelsea Arita <chelsea@chelseaarita.com>
2015-06-29 20:07:59 -0700

    wrote more code

--- adventure.py
+++ adventure.py
@@ -17,7 +17,7 @@ print "                                                         "
 print "                                                         "
 print "                                                         "
 print "========================================================="
-print "                Chelsea's final Project                  "
+print "                Chelsea's final project                  "
 print "========================================================="
 print "                                                         "
 print "                                                         "
@@ -42,9 +42,9 @@ while current<len(mylist):
 	else:
 		myinput = raw_input(mylist[current][0])
 		if myinput == 'keep walking':
-			print "You keep walking"
-		elif myinput == 'get closer':
-			print "you walk closer"
+			print "You keep walking."
+		elif myinput == 'walk closer':
+			print "You and your friends walk closer to the building as leaves and sticks crunch beneath your shoes."
 		current += 1
 	
 
@@ -52,6 +52,49 @@ while current<len(mylist):
 #for curr_list in mylist:
 #	myinput = raw_input(curr_list)#
 #	print myinput
+
+
+def choosePath(numberOfPaths):
+    choice = 0
+    while choice < 1 or choice > numberOfPaths:
+        # print('1 to ' + str(numberOfPaths) + '> ', end='')
+        choice = input()
+        if choice != '1' and choice != '2' and choice != '3' and choice != '4' and choice != '5':
+            choice = 0
+        if choice == '1' or choice == '2' or choice == '3' or choice == '4' or choice == '5':
+            choice = int(choice)
+    print()
+    return choice
+
+def choosePath():
+
+
+def intro():
+	print("You and your friends are walking through the Presido during dusk.")
+	print("The atmosphere is quiet as you hear the ocean waves break onto shore.")
+	print("You all come across an abandoned hospital.")
+	print("")
+	print("What will you do?")
+	print(" 1 Go inside of the abandoned hopsital.")
+	print(" 2 Keep walking on the path.")
+
+
+def front():
+	print("You are standing in front of the hospital. It's dark and uninviting.")
+	print("There is a repulsive stench of mildew, mold, and sulpher.")
+	print("There is also movement and errie cries coming from unrecgonizable animals.")
+	print("")
+	print("What do you do?")
+	print(" 1 Walk through the hallways.")
+	print(" 2 Not going to take any chances and leave.")
+
+def path():
+	print("You and your friends continue walking the path in the Presidio.")
+	print("You enjoy their random stories and company and would have not thought twice about walking into an abandoned building.")
+	print("	")
+
+#start the game
+intro()
 
 
 
