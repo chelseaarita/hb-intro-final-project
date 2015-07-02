11286e16d8c8416a06e8c9ea9c5fcc7859af9394
Chelsea Arita <chelsea@chelseaarita.com>
2015-07-01 20:28:47 -0700

    Cleaned up a bit of code and added more lines to the game.

--- adventure.py
+++ adventure.py
@@ -81,14 +81,16 @@ def choosePath(numberOfPaths):
 
 
 def intro():
-	print("                                                                 ")
-	print("You and your friends are walking through the Presido during dusk.")
+	print(" ")
+	print("You and your friends are walking through the Presidio during dusk.")
 	print("The atmosphere is quiet as you hear the ocean waves break onto shore.")
 	print("You all come across an abandoned hospital.")
-	print("")
+	print(" ")
 	print("What will you do?")
+	print(" ")
 	print(" 1 Walk inside the abandoned hopsital.")
 	print(" 2 Keep walking on the path.")
+	print(" ")
 
 	walk = choosePath(2)
 
@@ -98,27 +100,29 @@ def intro():
 		path()
 
 
-
 def front():
-	print("                                                                    ")
+	print(" ")
 	print("You are standing in front of the hospital. It's dark and uninviting.")
 	print("There is a repulsive stench of mildew, mold, and sulpher.")
 	print("There is also movement and errie cries coming from unrecgonizable animals.")
-	print("")
+	print(" ")
 	print("What do you do?")
+	print(" ")
 	print(" 1 Walk through the hallways.")
 	print(" 2 Not going to take any chances and leave.")
+	print(" ")
 
 	walk = choosePath(2)
 
 	if walk == 1:
 		hallways()
 	else:
-		gohome()
+		scared()
 
 def hallways():
-	print("                                                                                  ")
+	print(" ")
 	print("You're standing in long endless hallway and there are a dozen closed doors that see. What door do you go into?")
+	print(" ")
 
 	walk = choosePath(12)
 
@@ -128,45 +132,75 @@ def hallways():
 		stuck_door()
 
 def zombie():
-	print("                 ")
-	print("Aaah its a zombie")
-
-def stuck_door():
-	print("                         ")
-	print("bummer the door is stuck")
-	hallways()
-
-def gohome():
-	print("             ")
-	print("you went home")
-
-def path():
-	print("                                                               ")
-	print("You and your friends continue walking the path in the Presidio.")
-	print("You enjoy their random stories and company and would have not thought twice about walking into an abandoned building.")
-	print("	")
-	print("What will you do?")
-	print(" 1 Walk inside the abandoned hopsital.")
-	print(" 2 Keep walking on the path.")
-	print(" 3 just go home")
+	print(" ")
+	print("The door is hard to open from being stagnant for so long. You give the door a bigger push. After struggling for a moment, you finally get it open. But wait. What was that noise?")
+	print("It's now silent but you chills just ran down your spine")
+	print("You hear the same weird noise. You cower.")
+	print("You shine the flashlight onto the movement.")
+	print("It's a zombie coming right at you!")
+	print(" ")
+	print("What do you do?")
+	print(" ")
+	print(" 1 Defend yourself")
+	print(" 2 Stand there because you're in shock")
+	print(" 3 Run for your life")
+	print(" ")
 
 	walk = choosePath(3)
 
 	if walk == 1:
-		front()
+		defend()
 	elif walk == 2:
-		path()
+		shocked()
+	elif walk == 3:
+		run()
 	else:
-		go_home()
+		gohome()
 
-def go_home():
-	print("                 ")
-	print("I just went home")
+def defend():
+	print(" ")
+	print("You take a swing, the zombie jumps back and you miss")
+	print("The zombie lunges at you. You die.")
+	print(" ")
 
+def shocked():
+	print(" ")
+	print("You're so scared and still in shocked. You are not believing what you are seeing.")
+	print("The zombie lunges at you. You die.")
+	print(" ")
+
+def run():
+	print(" ")
+	print("You run out of the room and down the hallway.")
+	print("You're back to where you began.")
+	print(" ")
+	front()
+
+def stuck_door():
+	print(" ")
+	print("Bummer, the door is stuck.")
+	print(" ")
+	hallways()
+
+def gohome():
+	print(" ")
+	print("You decided to go home.")
+	print(" ")
+
+def path():
+	print(" ")
+	print("You and your friends continue walking the path in the Presidio.")
+	print("Walking through the park and hearing the waves crash onto shore is peaceful and comforting.")
+	print("You enjoy their random stories and company and would have not thought twice about walking into an abandoned building.")
+	print("	")
+
+def scared():
+	print(" ")
+	print("You're so scared. You run so fast, you end up tripping on debris, and falling. You quickly get up and bump your head. You were never very graceful when you were scared.")
+	print("You manage to limp away while holding your head, and you see the opening of the hopstial doors. You're so relieved.")
+	print("You leave the hospital and run off, never returning.")
+	print(" ")
 
 
 #start the game
 intro()
-
-
-
